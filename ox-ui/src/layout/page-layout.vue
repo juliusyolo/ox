a01359dd0fcecff12b5baf64beff369c30900a80
4defaa <xiaoshengsheng1994@gmail.com>
2024-05-21 23:38:23 +0800

    add ox-ui modules

---
+++ ox-ui/src/layout/page-layout.vue
@@ -0,0 +1,52 @@
+<script lang="ts">
+import type {CSSProperties} from 'vue';
+import {defineComponent} from 'vue'
+
+const headerStyle: CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: '#7dbcea',
+};
+
+const contentStyle: CSSProperties = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#108ee9',
+};
+
+const siderStyle: CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#3ba0e9',
+};
+
+const footerStyle: CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#7dbcea',
+};
+export default defineComponent({
+  name: "page-layout.vue",
+  components: {}
+})
+</script>
+
+<template>
+  <a-layout>
+    <a-layout-header :style="headerStyle">Header</a-layout-header>
+    <a-layout-content :style="contentStyle">
+      <router-view/>
+    </a-layout-content>
+    <a-layout-footer :style="footerStyle">Footer</a-layout-footer>
+  </a-layout>
+</template>
+
+<style scoped>
+
+</style>
---
+++ ox-ui/src/pages/customer/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import PageLayout from '../../layout/page-layout.vue'
+
+export default defineComponent({
+  name: "index.vue",
+  components: {PageLayout}
+})
+</script>
+
+<template>
+<page-layout>
+
+</page-layout>
+</template>
+
+<style scoped>
+
+</style>
