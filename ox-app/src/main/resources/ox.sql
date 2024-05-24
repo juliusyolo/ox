5c457b7f1074cfc68693774fd01eb9549ce124d4
4defaa <xiaoshengsheng1994@gmail.com>
2024-05-24 08:57:06 +0800

    add ox.sql

---
+++ ox-app/src/main/resources/ox.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql_3307
+ Source Server Type    : MySQL
+ Source Server Version : 80100 (8.1.0)
+ Source Host           : localhost:3307
+ Source Schema         : ox
+
+ Target Server Type    : MySQL
+ Target Server Version : 80100 (8.1.0)
+ File Encoding         : 65001
+
+ Date: 24/05/2024 08:56:12
+*/
+
